acee3d641e9fa65e744357788b05f3b73650fb5d
NuNuAhihi <phanthicam2196@gmail.com>
2018-03-14 16:22:02 +0700

    demo

---
+++ app/src/main/java/com/example/mydemo/ShowActivity.java
@@ -0,0 +1,17 @@
+package com.example.mydemo;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Admin on 10/3/2018.
+ */
+
+public class ShowActivity extends AppCompatActivity{
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+}
